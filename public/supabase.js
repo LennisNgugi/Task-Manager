c24967f919f8adcd0af14a3eaca37584b56da567
734415 <42470457+LennisNgugi@users.noreply.github.com>
2026-03-23 14:05:07 +0000

    Update supabase.js
    Updated to use .env instead of hard coding SUPABASE_URL and SUPABASE_ANON_KEY

--- public/supabase.js
+++ public/supabase.js
@@ -1,7 +1,7 @@
 // supabase.js
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
+import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = 'https://qsrslzddmydztzephubx.supabase.co'
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFzcnNsemRkbXlkenR6ZXBodWJ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg5MjM5MzcsImV4cCI6MjA3NDQ5OTkzN30.lBdehRWtEdLSHslZe8YZ_MpOU2GD9rT8Llq9_ztYEkM'
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
