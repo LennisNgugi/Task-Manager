216c19f2fdc82d9bb5c36fa5894ecd5d4d07f3fd
734415 <42470457+LennisNgugi@users.noreply.github.com>
2026-03-23 16:51:33 +0000

    Update supabase.js
    Comment solution for login issue.

--- public/supabase.js
+++ public/supabase.js
@@ -1,7 +1,11 @@
 // supabase.js
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL
-const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY
+const SUPABASE_URL = process.env.SUPABASE_URL
+const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY
+// Use hard coded supabase credentials below if .env is not allowing you to log into your account. 
+// Comment out the above const SUPABASE_URL = process.env.SUPABASE_URL and const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY
+//const SUPABASE_URL = 'your_supabase_url_here'
+//const SUPABASE_ANON_KEY = 'your_anon_key_here'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
